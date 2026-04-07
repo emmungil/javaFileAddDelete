3fe7c61a83b40f449e7d7d51ff1bbabe62b2059f
emmungil <emmungil@yahoo.com>
2026-04-08 00:20:26 +0300

    commit 1 from github desktop

--- FileWrite.java
+++ FileWrite.java
@@ -7,7 +7,7 @@ public class FileWrite
       try 
       { 
          FileWriter fw = new FileWriter("results.txt"); 
-         fw.write("hellos mellos"); 
+         fw.write("hellos mellos tellos"); 
          fw.close(); 
          System.out.println("\nContent written to the file successfully."); 
       } 
