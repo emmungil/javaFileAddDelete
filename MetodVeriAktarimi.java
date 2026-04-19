8df00adff2f3434475c88c25b745dd4dadaa2cb6
Levent <140748430+emmungil@users.noreply.github.com>
2026-04-19 22:07:12 +0300

    Create MetodVeriAktarimi.java

---
+++ MetodVeriAktarimi.java
@@ -0,0 +1,38 @@
+public class MetodVeriAktarimi { 
+
+// 1. metod: Rastgele sayı üretir 
+public static int rastgeleSayiUret() { 
+    int sayi = (int)(Math.random() * 100) + 1; // 1-100 arası 
+    System.out.println("Üretilen sayı: " + sayi); 
+    return sayi; 
+} 
+ 
+// 2. metod: Gelen sayının karesini hesaplar 
+public static int kareHesapla(int sayi) { 
+    int kare = sayi * sayi; 
+    System.out.println(sayi + " sayısının karesi: " + kare); 
+    return kare; 
+} 
+ 
+// 3. metod: Gelen sayının küpünü hesaplar 
+public static int kupHesapla(int sayi) { 
+    int kup = sayi * sayi * sayi; 
+    System.out.println(sayi + " sayısının küpü: " + kup); 
+    return kup; 
+} 
+ 
+public static void main(String[] args) { 
+    // 1. metodun ürettiği sayıyı al 
+    int uretilenSayi = rastgeleSayiUret(); 
+     
+    // Bu sayıyı diğer metoda gönder 
+    int kareSonuc = kareHesapla(uretilenSayi); 
+     
+    // kareSonuc'u başka metoda gönder 
+    int kupSonuc = kupHesapla(kareSonuc); 
+     
+    System.out.println("\nİşlem zinciri tamamlandı!"); 
+} 
+  
+
+} 
